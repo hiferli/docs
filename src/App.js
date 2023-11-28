a74713751d99ccdff27f2cab33eedbc25986ae86
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-11-28 19:42:56 +0530

    Created Boilerplate Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+	const storeKey = "notepad";
+	const [text, setText] = useState("");
+
+	useEffect(() => {
+		if(localStorage.getItem(storeKey)){
+			setText(localStorage.getItem(storeKey));
+		}
+	}, [])
+
+	const handleChange = (event) => {
+		localStorage.setItem(storeKey , event.target.value);
+		console.log(localStorage.getItem(storeKey))
+	}
+	
 	return (
-		<>
-			
-		</>
+		<div className='App'>
+			<textarea name="nodepad" id="notepad" defaultValue={text} onChange={(event) => handleChange(event)}></textarea>
+		</div>
 	);
 }
 
