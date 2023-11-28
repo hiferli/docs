66975d478b99a932ecef0b32c42998803e58ade0
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-11-29 00:48:51 +0530

    Implementing Timestamps

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
 	const storeKey = "notepad";
-	const [text, setText] = useState("");
+	const [text, setText] = useState({});
 
 	useEffect(() => {
 		if(localStorage.getItem(storeKey)){
-			setText(localStorage.getItem(storeKey));
+			const history = JSON.parse(localStorage.getItem(storeKey)); 
+			setText(history);
+			document.getElementById('currentTimestamp').innerText = history.timestamp
 		}
 	}, [])
 
 	const handleChange = (event) => {
-		localStorage.setItem(storeKey , event.target.value);
-		console.log(localStorage.getItem(storeKey))
+		const timestamp = new Date().toISOString().replace("T", " ").substring(0, 19);
+		localStorage.setItem(storeKey , JSON.stringify({
+			"text": event.target.value , 
+			"timestamp": timestamp 
+		}));
+
+		document.getElementById('currentTimestamp').innerText = timestamp;
 	}
 	
 	return (
 		<div className='App'>
-			<textarea name="nodepad" id="notepad" defaultValue={text} onChange={(event) => handleChange(event)}></textarea>
+			<textarea name="nodepad" id="notepad" defaultValue={text.text} onChange={(event) => handleChange(event)}></textarea>
+			<p className="timestamp" style={{color: 'grey', fontFamily: 'monospace'}}>Last Saved: <span id='currentTimestamp'></span></p>
 		</div>
 	);
 }
