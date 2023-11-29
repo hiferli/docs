08cce8835e5ad20b9e8dac9be8272f41787b7f40
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-11-29 13:15:53 +0530

    Connecting Database

--- Database/Server.js
+++ Database/Server.js
@@ -1,15 +1,29 @@
 import express, { json } from 'express';
 import cors from 'cors'
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors())
 app.use(json())
 
-const PORT = process.env.PORT || 2000;
+const PORT = 2000;
+const URI = 'mongodb://0.0.0.0:27017';
+
+const start = async () => {
+    try {
+        await mongoose.connect(
+            URI
+        );
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+start();
 
 app.get('/', async (req, res) => {
     res.json({ status: true, message: "Our node.js app works" })
 });
-
-app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
-
